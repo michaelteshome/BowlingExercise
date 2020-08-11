1e901072c2f6de2c1aefa167d639d0c7ccb65ad5
Michael Teshome <35178796+michaelteshome@users.noreply.github.com>
2020-08-10 23:31:18 -0400

    Add files via upload

---
+++ bowling_exercise.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void tokenize_input(char *input, int length);
+int charToInt(char val);
+
+int main(int argc, char **argv){
+
+
+    int n = strlen((const char*)"5/-5/-5/-5/-5/-5/-5/-5/-5/-5/-5");
+    int size = n;
+
+
+    char *input = malloc(size * sizeof(char));
+
+    if (input == NULL){
+        printf("error allocating memory for this pointer");
+        exit(0);
+    }
+
+    strcpy(input, "5/-5/-5/-5/-5/-5/-5/-5/-5/-5/-5");
+
+    tokenize_input(input, size);
+
+    free(input);
+
+    return 0;
+}
+
+
+void tokenize_input(char * input, int length){
+    const char *sequence = "-";
+    int sum = 0;
+    int strike = 0;
+
+    char *token = malloc(length * sizeof(char));
+
+    token = strtok(input, sequence);
+
+    while(token != NULL){
+        printf("%s\n", token);
+
+        //Check if all bowling input pin is of length 1: ex- X-X-X-X-X-X-X
+
+        if(strlen(token) == 1){
+
+            if(token[0] == 'X'){
+                sum += 20;
+                strike = 1;
+                token = strtok(NULL, sequence);
+                if(token == NULL){
+                    sum += 10;
+                    break;
+                }
+                else if(strlen(token) == 1){
+                    sum += 10;
+                }
+                else if(strlen(token) == 2){
+                    int b = charToInt(token[0]);
+                    int c = charToInt(token[1]);
+                    sum += b + c;
+                }
+                
+            }
+        }
+        //Check if the bowling input pin is of length 2: ex- 5/-5/-5/-5/-5/
+        else if(strlen(token) == 2){    
+            if(token[0] == 'X' || token[1] == 'X'){
+                strike = 1;
+                sum += charToInt(token[0]) + charToInt(token[1]);
+                token = strtok(NULL, sequence);
+
+                if(token == NULL){
+                    break;
+                }
+                else{
+                    sum += token[0];
+                }
+                //sum += (token == NULL) ? 0 : token[0];
+            }
+            else{
+                int a = charToInt((char)token[0]);
+                int b = charToInt((char)token[1]);
+                int c = a + b;
+                sum += c;   
+            }
+            
+        }
+        else if(token == NULL){
+            printf("\n null\n");
+            break;
+        }
+
+        //token = strtok(NULL, sequence);
+        token = (strike == 0) ? strtok(NULL, sequence) : token;
+        strike = (strike == 1) ? 0 : 0;
+    }
+
+    printf("Sum: %d\n", sum);
+    
+}
+
+
+int charToInt(char val){
+    if(val == '0'){
+        return 0;
+    }
+    else if(val == '1'){
+        return 1;
+    }
+    else if(val == '2'){
+        return 2;
+    }
+    else if(val == '3'){
+        return 3;
+    }
+    else if(val == '4'){
+        return 4;
+    }
+    else if(val == '5'){
+        return 5;
+    }
+    else if(val == '6'){
+        return 6;
+    }
+    else if(val == '7'){
+        return 7;
+    }
+    else if(val == '8'){
+        return 8;
+    }
+    else if(val == '9'){
+        return 9;
+    }
+    else if((int)val == 47){
+        return 10;
+    }
+    else if(val == 'X'){
+        return 10;
+    }
+    
+}
